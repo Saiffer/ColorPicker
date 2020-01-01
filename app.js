f4f7db659be31a5edd4832a132e6c0658efd3394
Saiffer <32097584+Saiffer@users.noreply.github.com>
2020-01-01 13:44:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
 /* jshint esversion: 6 */
-let a, b, c;
 
 function clickButton() {
     let first = Math.floor(Math.random() * 256);
     let second = Math.floor(Math.random() * 256);
     let third = Math.floor(Math.random() * 256);
-    console.log(first, second,third);
     return [first,second,third];
 }
 
 function getColorValue() {
     let values = clickButton();
     let rgb = `rgb(${values[0]}, ${values[1]}, ${values[2]})`;
-    console.log(rgb);
     return rgb;
 }
-
-
-let buttonClick = document.querySelector('.submit');
-
-
 
 function appendColor() {
     let rgb = getColorValue();
     document.querySelector('.color-box').style.backgroundColor = rgb;
-
 }
 
+let buttonClick = document.querySelector('.submit');
 buttonClick.addEventListener('click', appendColor, true);
